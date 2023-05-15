ab010138adb37354d9951ad1a623a4a22ff8b37d
Arie Trouw <arie.trouw@xylabs.com>
2023-05-15 16:33:59 -0700

    blog reference removed

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -108,10 +108,6 @@ const config = {
             title: 'More',
             items: [
               {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
                 label: 'GitHub',
                 href: 'https://github.com/XYOracleNetwork',
               },
