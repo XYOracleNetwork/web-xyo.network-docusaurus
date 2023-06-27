5f605113d34b4d67624b8aa9115618fe7e0d6d23
Arie Trouw <arie.trouw@xylabs.com>
2023-06-27 16:26:59 -0700

    smurf

--- docs/demo-dapps/GasPriceAggregator/GasPriceAggregatorDemo.tsx
+++ docs/demo-dapps/GasPriceAggregator/GasPriceAggregatorDemo.tsx
@@ -1,4 +1,4 @@
-import { XyoEmbedPluginCard } from '@xyo-network/react-embed'
+import { EmbedPluginCard } from '@xyo-network/react-embed'
 import { EthereumGasPricePlugin } from '@xyo-network/react-plugins'
 import React from 'react'
 
@@ -6,5 +6,5 @@ import { useFetchPointerHash } from './hooks'
 export const GasPriceAggregatorDemo: React.FC = () => {
   const GasAggregatePricePointer = useFetchPointerHash('EthereumGasPrice')
 
-  return <XyoEmbedPluginCard huriPayload={GasAggregatePricePointer} plugins={[EthereumGasPricePlugin]}></XyoEmbedPluginCard>
+  return <EmbedPluginCard huriPayload={GasAggregatePricePointer} plugins={[EthereumGasPricePlugin]}></EmbedPluginCard>
 }
