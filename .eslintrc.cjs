3439b3266d00b4d56fc8623591d509344b3a9fba
Arie Trouw <arie.trouw@xylabs.com>
2023-12-18 12:08:45 -0800

    Auto stash before merge of "main" and "origin/main"
    Partial Updo

---
+++ .eslintrc.cjs
@@ -0,0 +1,63 @@
+const finalConfig = {
+  "extends": ["@xylabs", "@xylabs/react"],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', project: null, tsconfigRootDir: __dirname, extraFileExtensions: ['json'] },
+  "root": true,
+  "ignorePatterns": [
+    "dist",
+    "node_modules",
+    "docs",
+    "coverage",
+    "docker",
+    "nftData",
+    "testData.json",
+    "storybook-static",
+    "*.stories.*",
+    "swagger.json",
+    ".yarn",
+    ".*"
+  ],
+  "rules": {
+    "@typescript-eslint/explicit-member-accessibility": ["warn", { "accessibility": "no-public" }],
+    "no-restricted-imports": [
+      "warn",
+      {
+        "paths": [
+          "@xyo-network/bridge",
+          "@xyo-network/core",
+          "@xyo-network/module",
+          "@xyo-network/modules",
+          "@xyo-network/node",
+          "@xyo-network/sdk",
+          "@xyo-network/plugins",
+          "@xyo-network/protocol",
+          "@xyo-network/witness",
+          "react-player",
+          "filepond",
+          "aos",
+          "react-icons",
+          ".",
+          "..",
+          "../..",
+          "../../..",
+          "../../../..",
+          "../../../../..",
+          "../../../../../..",
+          "../../../../../../.."
+        ]
+      }
+    ],
+    "import/no-internal-modules": [
+      "warn", {
+        "allow": [
+          "source-map-support/*",
+          "lodash/*",
+          "aws-sdk/**/*",
+          "types/*"
+        ]
+      }
+    ]
+  }
+}
+
+module.exports = finalConfig
