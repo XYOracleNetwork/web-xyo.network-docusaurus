b02c2f4a83947dd3825b2ce03a1a6ee26a3c9b0e
Arie Trouw <arie.trouw@xylabs.com>
2024-09-30 16:05:36 -0700

    Build fixed

--- src/theme/DocItem/Layout/index.tsx
+++ src/theme/DocItem/Layout/index.tsx
@@ -1,6 +1,5 @@
+import { useDoc } from '@docusaurus/plugin-content-docs/client'
 import { useWindowSize } from '@docusaurus/theme-common'
-
-import { useDoc } from '@docusaurus/theme-common/internal'
 import DocBreadcrumbs from '@theme/DocBreadcrumbs'
 import DocItemContent from '@theme/DocItem/Content'
 import DocItemFooter from '@theme/DocItem/Footer'
@@ -9,7 +8,6 @@ import DocItemTOCDesktop from '@theme/DocItem/TOC/Desktop'
 import DocItemTOCMobile from '@theme/DocItem/TOC/Mobile'
 import DocVersionBadge from '@theme/DocVersionBadge'
 import DocVersionBanner from '@theme/DocVersionBanner'
-
 import clsx from 'clsx'
 import React from 'react'
 
